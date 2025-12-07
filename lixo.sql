f5334e48f55df0025039f1e8f4eeb9a4ca75c907
Leonardo-Git <laam@aluno.ifnmg.edu.br>
2025-12-07 15:15:21 -0300

    add lixo.sql
    Create a new table 'lixocoletado' with relevant fields.

---
+++ lixo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `lixocoletado` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `data` date NOT NULL,
+    `horario` time NOT NULL,
+    `tipo` varchar(250) NOT NULL,
+    PRIMARY KEY (`id`)
+)
+USE lixo; -- ou o nome do banco configurado no db.js
