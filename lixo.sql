9ddef92fe836eec37c5d03d7e25f894e6359ff16
vitote <mcvitote@gmail.com>
2025-12-07 14:54:28 -0300

    add o banco de dados

---
+++ lixo.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE `lixocoletado` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `data` date NOT NULL,
+    `horario` time NOT NULL,
+    `tipo` varchar(250) NOT NULL,
+    PRIMARY KEY (`id`)
+)
+USE lixo;
